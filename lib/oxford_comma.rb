75f1c99ef07109aeb1c8fca3750f6045949d3d81
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-31 00:18:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 elsif array.length >= 3
-  array.join(", ")
   array[-1].prepend("and ")
-  array
-end
+
+  array.join(", ")
 end
