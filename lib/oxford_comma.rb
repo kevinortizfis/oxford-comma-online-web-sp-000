5020983208a00141ad44ae8338125988c6dda058
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-31 00:17:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length >= 3
+  array.join(", ")
+  for array[-1] in array
+    array << "and #{array[0]}"
+  end
+end
 end
